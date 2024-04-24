cc0813354041dc5317dfc9651c42f1d8f4835110
Marshall <42623637+marshalljwu@users.noreply.github.com>
2024-04-24 17:22:32 -0400

    Returning all data as list of list, changing to list of dictionaries

--- backend/server.py
+++ backend/server.py
@@ -1,15 +1,19 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, redirect
 import sqlite3
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return redirect('/hsr_stats_character')
+
 @app.route('/hsr_stats_character')
-def get_data():
+def hsr_stats_character():
     conn = sqlite3.connect('databaseMain.db')
     c = conn.cursor()
     c.execute('SELECT * FROM characters')
 
-    data = cursor.fetchall()
+    data = c.fetchall()
     conn.close()
     return jsonify(data)
 
