071da7b5e73c9a5487bc622a5da991fc49303a37
Marshall <42623637+marshalljwu@users.noreply.github.com>
2024-04-25 11:20:15 -0400

    Added RESTful functionality

--- backend/server.py
+++ backend/server.py
@@ -1,21 +1,33 @@
-from flask import Flask, jsonify, redirect
+from flask import Flask, jsonify, request, abort
 import sqlite3
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return redirect('/hsr_stats_character')
+@app.route('/hsr_stats_character', methods=['GET'])
+def get_character_stats():
+    data = get_all()
+    return jsonify(data)
 
-@app.route('/hsr_stats_character')
-def hsr_stats_character():
+def get_all():
     conn = sqlite3.connect('databaseMain.db')
     c = conn.cursor()
     c.execute('SELECT * FROM characters')
-
-    data = c.fetchall()
+    rows = c.fetchall()
     conn.close()
-    return jsonify(data)
+
+    stats = []
+    for row in rows:
+        stats.append({
+            'id': row[0],
+            'HP': row[1],
+            'ATK': row[2],
+            'DEF': row[3],
+            'SPD': row[4],
+            'CR': row[5],
+            'CD': row[6],
+            'UID': row[7],
+        })
+    return stats
 
 if __name__ == '__main__':
     app.run(debug = True)
