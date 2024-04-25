2dc1d96000a036d22d4ccec9fe19f04cc1f68d7e
Marshall <42623637+marshalljwu@users.noreply.github.com>
2024-04-25 11:24:35 -0400

    Added ability to retrieve by character id and proper error messages

--- backend/server.py
+++ backend/server.py
@@ -1,14 +1,28 @@
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, make_response
 import sqlite3
 
 app = Flask(__name__)
 
-@app.route('/hsr_stats_character', methods=['GET'])
-def get_character_stats():
-    data = get_all()
-    return jsonify(data)
+# Gets all characters in database
+@app.route('/hsr_character_stats', methods=['GET'])
+def get_stats_all():
+    data = getDatabaseAll()
+    if data:
+        return jsonify(data), 200
+    else:
+        return make_response(jsonify({'Error': 'Could not retrieve from database'}), 404)
 
-def get_all():
+
+# Gets characters by id
+@app.route('/hsr_character_stats/<int:character_id>', methods=['GET'])
+def get_stats_id(character_id):
+    data = getDatabaseID(character_id)
+    if data:
+        return jsonify(data), 200
+    else:
+        return make_response(jsonify({'Error': 'Could not retrieve by id'}), 404)
+    
+def getDatabaseAll():
     conn = sqlite3.connect('databaseMain.db')
     c = conn.cursor()
     c.execute('SELECT * FROM characters')
@@ -28,6 +42,30 @@ def get_all():
             'UID': row[7],
         })
     return stats
+
+def getDatabaseID(character_id):
+    conn = sqlite3.connect('databaseMain.db')
+    c = conn.cursor()
+    c.execute('SELECT * FROM characters WHERE id = ?', (character_id,))
+    rows = c.fetchall()
+    conn.close()
+
+    if rows:
+        stats = []
+        for row in rows:
+            stats.append({
+                'id': row[0],
+                'HP': row[1],
+                'ATK': row[2],
+                'DEF': row[3],
+                'SPD': row[4],
+                'CR': row[5],
+                'CD': row[6],
+                'UID': row[7],
+            })
+        return stats
+    else:
+        return None
 
 if __name__ == '__main__':
     app.run(debug = True)
